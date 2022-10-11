444e252b43db277787e0d164608e4d71200254e9
Kartikey Mishr <kartikeymishr@hotmail.com>
2022-10-11 20:51:12 +0530

    Added Header

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,13 @@ import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
+                <Header/>
                 <div>
                     <Route path="/" exact component={StreamList}></Route>
                     <Route path="/stream/new" exact component={StreamCreate}></Route>
