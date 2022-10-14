f39f1d4ddcaec40bea8ee1483a0c950416f403f5
Kartikey Mishr <kartikeymishr@hotmail.com>
2022-10-14 17:52:31 +0530

    Hooking up Create Stream action and adding all available endpoints as Actions.

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 import {Field, reduxForm} from "redux-form";
+import {connect} from "react-redux";
+import {createStream} from "../../actions/actions";
 
 class StreamCreate extends Component {
     renderError({error, touched}) {
@@ -25,8 +27,10 @@ class StreamCreate extends Component {
         )
     }
 
-    onSubmit(formValues) {
+    onSubmit = (formValues) => {
         console.log(formValues);
+
+        this.props.createStream(formValues)
     }
 
     render() {
@@ -56,7 +60,9 @@ const validate = (formValues) => {
     return errors
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate)
+
+export default connect(null, {createStream})(formWrapped)
